31da0ac80558d839e8423bea571f0aa5cc8b7ca2
thomas <dahlsbiavl@yahoo.com>
2024-09-19 14:39:34 +0200

    added a mysql class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,6 +7,17 @@
         Admin
         <small>Subheading</small>
       </h1>
+
+      <?php
+
+      $sql = "SELECT * FROM users WHERE id=1";
+      $result = $database->query($sql);
+      $user_found = mysqli_fetch_array($result);
+      echo $user_found['first_name'];
+      print_r ($user_found);
+
+      ?>
+
       <ol class="breadcrumb">
         <li>
           <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
